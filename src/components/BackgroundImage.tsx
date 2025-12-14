6555046ff404bc2f8b24801f0c3b97160d028306
Yorgan-Agb <agblemagnon.yorgan@gmail.com>
2025-12-14 20:55:26 +0100

    npm i tailwind + début compososant background

---
+++ src/components/BackgroundImage.tsx
@@ -0,0 +1,7 @@
+export const BackgroundImage = () => {
+  return (
+    <div className="imgContainer">
+      <section className="bg-[url('/lofi-1.jpg')] h-screen bg-no-repeat"></section>
+    </div>
+  );
+};
