f8bced7e250c6e524072d2c260c45b5a05e8130e
Yorgan-Agb <agblemagnon.yorgan@gmail.com>
2025-12-15 16:29:50 +0100

    style + link for the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,34 @@
-import { LuGithub } from "react-icons/lu";
-import { LuLinkedin } from "react-icons/lu";
+import { LuGithub, LuLinkedin, LuEarth } from "react-icons/lu";
 
 export const Header = () => {
   return (
-    <header className="relative flex items-center justify-between z-50">
-      <h1 className="font-lofi text-white">3AM.FM</h1>
-      <div className="flex">
-        <LuGithub className="text-white" />
-        <LuLinkedin className="text-white" />
-      </div>
+    <header className="relative p-4 flex items-center justify-between z-50">
+      <h1 className="font-lofi text-2xl text-gray-700 hover:text-purple-300">
+        3AM.FM
+      </h1>
+      <nav className="flex gap-2">
+        <a
+          href="https://yorgan-portfolio.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LuEarth className="text-gray-700 hover:text-purple-200 text-4xl" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/yorgan-agblemagnon/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LuLinkedin className="text-gray-700 hover:text-purple-200 text-4xl" />
+        </a>
+        <a
+          href="https://github.com/Yorgan-Agb"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LuGithub className="text-gray-700 hover:text-purple-200 text-4xl" />
+        </a>
+      </nav>
     </header>
   );
 };
