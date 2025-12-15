3e1fc0607e2d445a4c531d25fb3126e840d956e0
Yorgan-Agb <agblemagnon.yorgan@gmail.com>
2025-12-15 01:01:15 +0100

    create state for the transition of all img + improvement of the useEffect component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionDuration: {
+
+        '2000': "2000ms",
+        '3000': '3000ms',
+      }
+    },
   },
   plugins: [],
 }
